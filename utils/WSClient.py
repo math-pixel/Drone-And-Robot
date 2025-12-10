67e839fc94da0619650572a184b900b9b4a1ec85
math-pixel <59537610+math-pixel@users.noreply.github.com>
2025-12-10 17:39:51 +0100

    create main activity

--- utils/WSClient.py
+++ utils/WSClient.py
@@ -1,9 +1,15 @@
+# test_client.py
+import asyncio
+import websockets
+import json
+
+config = """
 {
     "name": "global_data_transfer",
     "version": "1.0.0",
-    "sequencing" : 0,
+    "sequencing" : 1,
     "activity_atmosphere": {
-        "ws_server_address": "ws://",
+        "ws_server_address": "ws://localhost:8765",
         "dmx": [
             {"lamp1": 
                 {"channel": 1, "value": 255}
@@ -71,3 +77,21 @@
         "step_of_activities": 0
     }
 }
+"""
+
+async def ws_send_to(uri = "ws://localhost:8765", data="Salut serveur!"):
+    uri = uri
+    async with websockets.connect(uri) as websocket:
+        # 1. Message normal
+        await websocket.send("Salut serveur!")
+        
+        # 2. Le message spécial JSON
+        data = data
+        await websocket.send(json.dumps(data))
+        
+        # Lire la réponse du process
+        response = await websocket.recv()
+        print(f"Réponse du serveur: {response}")
+
+if __name__ == "__main__":
+    asyncio.run(ws_send_to(uri="ws://localhost:8765", data=json.loads(config)))
