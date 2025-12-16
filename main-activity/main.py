bdefbaad147b5cb5596613bb488b5cae7f13d2c8
Thibaud <obit2004@hotmail.com>
2025-12-16 09:05:36 +0100

    IOS swift

--- main-activity/main.py
+++ main-activity/main.py
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     config_path = os.path.join(parent_dir, "config.json")
     print(f"path du config: {config_path}")
     config_ws = {
-        "host": "192.168.10.50",
+        "host": "192.168.1.13",
         "port": 8005
     }
     server = AdvancedWSServer(delegate=WsDelegate(), config=config_ws)
