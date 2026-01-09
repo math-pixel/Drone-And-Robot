a423766adc5b8a9c727252b6ba5360481b6da642
Thibaud <thibaud.evrard@cern.ch>
2026-01-09 14:09:18 +0100

    mom fix

--- utils/test_websocket.py
+++ utils/test_websocket.py
@@ -39,9 +39,9 @@ if __name__ == "__main__":
         x = 0
         while True:
             ### Emotion levels example
-            d_h, d_s, d_sh, d_a = _ask_4_numbers()
-            k = "update_emotions"
-            client.set_emotion_levels(d_h, d_s, d_sh, d_a)
+            # d_h, d_s, d_sh, d_a = _ask_4_numbers()
+            # k = "update_emotions"
+            # client.set_emotion_levels(d_h, d_s, d_sh, d_a)
             
             ### Jauge score example
             # k = "update_jauge_score"
@@ -54,20 +54,20 @@ if __name__ == "__main__":
             # await client._send_json(key=key)
 
             ### Stepper motor example
-            # input("Press Enter to continue...")
             # # k = "mom_activity_stepper_control_turn_right_90"
             # # k = "mom_activity_stepper_control_init_position"
-            # x += 90
-            # k = f'mom_activity_stepper_{x}'
+            input("Press Enter to continue...")
+            x += 90
+            k = f'mom_activity_stepper_{x}'
 
-            k = "choice_activity_step_1_finished"
+            # k = "choice_activity_step_1_finished"
 
             await client._send_json(key=k)
         
 
     client = WSClient(
             url="ws://192.168.10.34:8057/ws",
-            client_key="throw_activity",
+            client_key="atmosphere_activity",
             action_delegate=my_action_handler,
             steps=STEPS
         )
